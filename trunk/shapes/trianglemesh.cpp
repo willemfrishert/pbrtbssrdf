2082d4334f2f90e9309a4cbe4d7f7e4bf8fb1d7a
jpjorge <jpjorge@fc80425e-dd24-0410-9c55-b78c9d0b4a36>
2007-01-11 09:51:00 +0000

    --> Added GetDifferentialGeometry and the cpp file was cut to create the header

--- trunk/shapes/trianglemesh.cpp
+++ trunk/shapes/trianglemesh.cpp
@@ -1,177 +1,10 @@
 
-/*
- * pbrt source code Copyright(c) 1998-2005 Matt Pharr and Greg Humphreys
- *
- * All Rights Reserved.
- * For educational use only; commercial use expressly forbidden.
- * NO WARRANTY, express or implied, for this software.
- * (See file License.txt for complete license)
- */
-
 // trianglemesh.cpp*
 #include "shape.h"
 #include "paramset.h"
 
 #include "core/sampling.h"
-
-#include "PointRepulsion.h"
-
-// ###################### TriangleMesh class #############################
-class TriangleMesh : public Shape 
-{
-public:
-	// TriangleMesh Public Methods
-	TriangleMesh(const Transform &o2w, bool ro,
-	             int ntris, int nverts, const int *vptr,
-				 const Point *P, const Normal *N,
-				 const Vector *S, const float *uv);
-	~TriangleMesh();
-
-//	virtual void GetUniformPointSamples(vector<Point>& container) const;
-	virtual void GetUniformPointSamples(vector<std::pair<Point, Normal > >& container) const;
-	BBox ObjectBound() const;
-	BBox WorldBound() const;
-	bool CanIntersect() const { return false; }
-
-	void Refine(vector<Reference<Shape> > &refined) const;
-
-	friend class Triangle;
-	template <class T> friend class VertexTexture;
-
-protected:
-	// TriangleMesh Data
-	int ntris;			// Number of Triangles
-	int nverts;			// Number of Vertices
-	int *vertexIndex;	// Triangles
-	Point *p;			// Vertices
-	Normal *n;			// Normals
-	Vector *s;
-	float *uvs;
-};
-
-// ###################### Triangle class #############################
-class Triangle : public Shape 
-{
-public:
-
-	// Triangle Public Methods
-	Triangle(const Transform &o2w, bool ro, TriangleMesh *m, int n)
-	: Shape(o2w, ro) 
-	{
-		mesh = m;
-		v = &mesh->vertexIndex[3*n];
-		// Update created triangles stats
-		static StatsCounter trisMade("Geometry", "Triangles created");
-		++trisMade;
-	}
-
-	BBox ObjectBound() const;
-	BBox WorldBound() const;
-
-	bool Intersect(const Ray &ray, float *tHit,
-	               DifferentialGeometry *dg) const;
-	bool IntersectP(const Ray &ray) const;
-
-	void GetUVs(float uv[3][2]) const;
-
-	float Area() const;
-
-	virtual void GetShadingGeometry(const Transform &obj2world,const DifferentialGeometry &dg,DifferentialGeometry *dgShading) const 
-	{
-		if (!mesh->n && !mesh->s) 
-		{
-			*dgShading = dg;
-			return;
-		}
-
-		// Initialize _Triangle_ shading geometry with _n_ and _s_
-		// Compute barycentric coordinates for point
-		float b[3];
-		// Initialize _A_ and _C_ matrices for barycentric
-		float uv[3][2];
-		GetUVs(uv);
-		float A[2][2] ={ { uv[1][0] - uv[0][0], uv[2][0] - uv[0][0] },
-						 { uv[1][1] - uv[0][1], uv[2][1] - uv[0][1] } };
-		float C[2] = { dg.u - uv[0][0], dg.v - uv[0][1] };
-
-		if (!SolveLinearSystem2x2(A, C, &b[1])) 
-		{
-			// Handle degenerate parametric mapping
-			b[0] = b[1] = b[2] = 1.f/3.f;
-		}
-		else
-		{
-			b[0] = 1.f - b[1] - b[2];
-		}
-			
-		// Use _n_ and _s_ to compute shading tangents for triangle, _ss_ and _ts_
-		Normal ns;
-		Vector ss, ts;
-		if (mesh->n)
-		{
-			ns = Normalize(obj2world(b[0] * mesh->n[v[0]] +	b[1] * mesh->n[v[1]] + b[2] * mesh->n[v[2]]));
-		}
-		else 
-		{
-			ns = dg.nn;
-		}
-		
-		if (mesh->s)
-		{
-			ss = Normalize(obj2world(b[0] * mesh->s[v[0]] + b[1] * mesh->s[v[1]] + b[2] * mesh->s[v[2]]));
-		}
-		
-		else 
-		{
-			ss = Normalize(dg.dpdu);
-		}
-		
-		ts = Normalize(Cross(ss, ns));
-		ss = Cross(ts, ns);
-		Vector dndu, dndv;
-		if (mesh->n) 
-		{
-			// Compute \dndu and \dndv for triangle shading geometry
-			float uvs[3][2];
-			GetUVs(uvs);
-			// Compute deltas for triangle partial derivatives of normal
-			float du1 = uvs[0][0] - uvs[2][0];
-			float du2 = uvs[1][0] - uvs[2][0];
-			float dv1 = uvs[0][1] - uvs[2][1];
-			float dv2 = uvs[1][1] - uvs[2][1];
-			Vector dn1 = Vector(mesh->n[v[0]] - mesh->n[v[2]]);
-			Vector dn2 = Vector(mesh->n[v[1]] - mesh->n[v[2]]);
-			float determinant = du1 * dv2 - dv1 * du2;
-			if (determinant == 0)
-			{
-				dndu = dndv = Vector(0,0,0);
-			}
-			else 
-			{
-				float invdet = 1.f / determinant;
-				dndu = ( dv2 * dn1 - dv1 * dn2) * invdet;
-				dndv = (-du2 * dn1 + du1 * dn2) * invdet;
-			}
-		}
-		else
-		{
-			dndu = dndv = Vector(0,0,0);
-		}
-			
-		*dgShading = DifferentialGeometry(dg.p, ss, ts,	dndu, dndv, dg.u, dg.v, dg.shape);
-		dgShading->dudx = dg.dudx;  dgShading->dvdx = dg.dvdx; // NOBOOK
-		dgShading->dudy = dg.dudy;  dgShading->dvdy = dg.dvdy; // NOBOOK
-		dgShading->dpdx = dg.dpdx;  dgShading->dpdy = dg.dpdy; // NOBOOK
-	}
-
-	Point Sample(float u1, float u2, Normal *Ns) const;
-
-private:
-	// Triangle Data
-	Reference<TriangleMesh> mesh;
-	int *v;
-};
-
+#include "trianglemesh.h"
 
 // TriangleMesh Method Definitions
 // ###################### TriangleMesh Method Definitions #############################
@@ -451,6 +284,43 @@ bool Triangle::IntersectP(const Ray &ray) const
 	triangleHits.Add(1, 0); //NOBOOK
 
 	return true;
+}
+
+/**
+ * @param p
+ * @param dg
+ */
+void Triangle::GetDifferentialGeometry( const Point& p, DifferentialGeometry *dg )
+{
+	const Point &p1 = mesh->p[v[0]];
+	const Point &p2 = mesh->p[v[1]];
+	const Point &p3 = mesh->p[v[2]];
+	
+	// Fill in _DifferentialGeometry_ from triangle hit
+	// Compute triangle partial derivatives
+	Vector dpdu, dpdv;
+	float uvs[3][2];
+	GetUVs(uvs);
+	
+	GetPartialDerivatives(dpdu, dpdv, uvs, p1, p2, p3);
+
+	float A = p1.x - p3.x;
+	float B = p2.x - p3.x;
+	float C = p3.x - p.x;
+	float F_I = p3.y - p.y + p3.z - p.z;
+	float E_H = p2.y - p3.y + p2.z - p3.z;
+	float D_G = p1.y - p3.y + p1.z - p3.z;
+
+	float b1 = (B * F_I - C * E_H) / A * E_H - B * D_G;
+	float b2 = (A * F_I - C * D_G) / B * D_G - A * E_H;
+
+	// Interpolate $(u,v)$ triangle parametric coordinates
+	float b0 = 1 - b1 - b2;
+	float tu = b0*uvs[0][0] + b1*uvs[1][0] + b2*uvs[2][0];
+	float tv = b0*uvs[0][1] + b1*uvs[1][1] + b2*uvs[2][1];
+	*dg = DifferentialGeometry(p, dpdu, dpdv,
+		Vector(0,0,0), Vector(0,0,0),
+		tu, tv, this);
 }
 
 void Triangle::GetUVs(float uv[3][2]) const 
