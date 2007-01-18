b93544bfbc2fd51f78faedc9059624ba28bf6ca2
wfrishert <wfrishert@fc80425e-dd24-0410-9c55-b78c9d0b4a36>
2007-01-18 19:53:35 +0000

    Added Rotate function which takes an angle of rotation, a vector to rotate about and two Matrix4x4 arguments as input

--- core/transform.cpp
+++ core/transform.cpp
@@ -97,6 +97,38 @@ Transform Rotate(float angle, const Vector &axis) {
 	Matrix4x4 *mat = new Matrix4x4(m);
 	return Transform(mat, mat->Transpose());
 }
+
+void Rotate(float angle, const Vector &axis, Reference<Matrix4x4>& aM, Reference<Matrix4x4>& aMinv)
+{
+	Vector a = Normalize(axis);
+	float s = sinf(Radians(angle));
+	float c = cosf(Radians(angle));
+	float m[4][4];
+
+	m[0][0] = a.x * a.x + (1.f - a.x * a.x) * c;
+	m[0][1] = a.x * a.y * (1.f - c) - a.z * s;
+	m[0][2] = a.x * a.z * (1.f - c) + a.y * s;
+	m[0][3] = 0;
+
+	m[1][0] = a.x * a.y * (1.f - c) + a.z * s;
+	m[1][1] = a.y * a.y + (1.f - a.y * a.y) * c;
+	m[1][2] = a.y * a.z * (1.f - c) - a.x * s;
+	m[1][3] = 0;
+
+	m[2][0] = a.x * a.z * (1.f - c) - a.y * s;
+	m[2][1] = a.y * a.z * (1.f - c) + a.x * s;
+	m[2][2] = a.z * a.z + (1.f - a.z * a.z) * c;
+	m[2][3] = 0;
+
+	m[3][0] = 0;
+	m[3][1] = 0;
+	m[3][2] = 0;
+	m[3][3] = 1;
+
+	aM = new Matrix4x4(m);
+	aMinv = aM->Transpose();
+}
+
 Transform LookAt(const Point &pos, const Point &look, const Vector &up) {
 	float m[4][4];
 	// Initialize fourth column of viewing matrix
